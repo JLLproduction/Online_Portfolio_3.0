09cd88f9e899c860fcc71468e43c543213df6382
jackmasterflex <jlopez1992@yahoo.com>
2019-04-18 10:25:04 -0400

    built in lazy loading modules for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: './modules/home/home.module#HomeModule'
+  },
+  {
+    path: 'about',
+    loadChildren: './modules/about/about.module#AboutModule'
+  },
+  {
+    path: 'experience',
+    loadChildren: './modules/experience/experience.module#ExperienceModule'
+  },
+  {
+    path: 'projects',
+    loadChildren: './modules/projects/projects.module#ProjectsModule'
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
